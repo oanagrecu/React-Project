52364cf78a2e39ea044af8dbaadd48b54896a8be
oanagrecu <oanagrecu09@yahoo.com>
2023-09-11 12:02:54 +0200

    resolve issues

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-<<<<<<< HEAD
 import Navbar from "./common/Navbar.js";
-=======
-import Navbar from "./common/navbar.js";
->>>>>>> 35b36a978dbf4a25cdeee30ad3eb67de81c665b8
 import Midpart from "./homepage/midpart.js";
 import Loader from "./homepage/loader";
 import Show from "./homepage/show.js";
 import Change from "./homepage/imgchange.js";
 import Footer from "./common/footer.js";
 import Proudct from "./proudctpage/product.js";
-<<<<<<< HEAD
 import ProductProvider from "./proudctpage/ProductProvider";
 import ProductDetail from "./proudctpage/proudctData.js";
 import Minin from "./proudctpage/miniCart.js";
@@ -23,18 +18,10 @@ import "./common/footer.css";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Cart from "./pages/Cart";
-=======
-import ProductProvider from "./proudctpage/ProductProvider.js";
->>>>>>> 35b36a978dbf4a25cdeee30ad3eb67de81c665b8
-
-import ProductDetail from "./proudctpage/proudctData.js";
 import "./App.css";
 import "./common/navbar.css";
 import "./common/footer.css";
 // import FooterBottom from "./common/FooterBottom";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Cart from "./pages/Cart";
 function App() {
   const [isLoading, setIsLoading] = useState(true);
 
@@ -63,7 +50,6 @@ function App() {
                   <Midpart />
                   <Show />
                   <Footer />
-<<<<<<< HEAD
                   <Minin />
                 </>
               }
@@ -72,14 +58,6 @@ function App() {
             <Route path="/product/:id" element={<ProductDetail />} />
             <Route path="/cart" element={<Minin />} />
             <Route path="/test" element={<Testr />} />
-=======
-                  
-                </>
-              }
-            />
-            <Route path="/Collection" element={<Proudct />} />
-            <Route path="/Collection/:id" element={<ProductDetail />} />
->>>>>>> 35b36a978dbf4a25cdeee30ad3eb67de81c665b8
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/cart" element={<Cart />} />
