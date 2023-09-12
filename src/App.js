831d9def74c02bd0a1db16eac49685ba7c766c8e
root <raadsn11@gmail.com>
2023-09-12 16:44:43 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Navbar from "./common/Navbar.js";
-import Midpart from "./homepage/midpart.js";
-import LoaderComp from "./homepage/loader";
-import Show from "./homepage/show.js";
-import Change from "./homepage/imgchange.js";
-import Footer from "./common/footer.js";
-import Proudct from "./proudctpage/product.js";
-import ProductProvider from "./proudctpage/ProductProvider";
-import ProductDetail from "./proudctpage/proudctData.js";
-import Minin from "./proudctpage/miniCart.js";
-import CartShow from "./proudctpage/cartShow.js";
-import Testr from "./proudctpage/test.js";
-import "./App.css";
-import "./common/navbar.css";
-import "./common/footer.css";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Cart from "./pages/Cart";
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Navbar from './common/Navbar.js';
+import Midpart from './homepage/midpart.js';
+import Loader from './homepage/loader';
+import Show from './homepage/show.js'
+import Change from './homepage/imgchange.js'
+import Footer from './common/footer.js'
+import Proudct from './proudctpage/product.js'
+import ProductProvider from './proudctpage/ProductProvider'
+import ProductDetail from './proudctpage/proudctData.js'
+import Minin from './proudctpage/miniCart.js'
+import  CartShow from './proudctpage/cartShow.js';
+import  Testr from './proudctpage/test.js';
+import './App.css';
+import './common/navbar.css';
+import './common/footer.css';
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -31,38 +28,31 @@ function App() {
   }, []);
 
   if (isLoading) {
-    return <LoaderComp />;
+    return <Loader />;
   }
 
-  return (
-    <ProductProvider>
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <>
-                  <Midpart />
-                  <Show />
-                  <Footer />
-                  <Cart />
-                  <Minin />
-                </>
-              }
-            />
-            <Route path="/Collection" element={<CartShow />} />
-            <Route path="/product/:id" element={<ProductDetail />} />
-            {/* <Route path="/cart" element={<Minin />} /> */}
-            <Route path="/test" element={<Testr />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/cart" element={<Cart />} />
-          </Routes>
-        </div>
-      </Router>
-    </ProductProvider>
+ return (
+        <ProductProvider>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={
+            <>
+              <Midpart />
+              <Show />
+              <Footer />
+              <Minin />
+            </>
+          } />
+          <Route path="/Collection" element={<CartShow/>} />
+          <Route path="/product/:id" element={<ProductDetail />} />
+          <Route path="/cart" element={<Minin />} />
+          <Route path="/test" element={<Testr />} />
+        </Routes>
+      </div>
+    </Router>
+        </ProductProvider>
   );
 }
 
