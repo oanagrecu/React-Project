491e41118c23994bc7827dd860efd989924a50e7
root <raadsn11@gmail.com>
2023-09-14 11:45:43 +0200

    homepage fixed

--- src/homepage/show.js
+++ src/homepage/show.js
@@ -1,17 +1,19 @@
-import { useState, useEffect } from "react";
+import {useState, useEffect, useContext} from "react";
 import "./show.css";
 import jacket1 from '../img/jacket1.webp'
 import jacket2 from '../img/jacket2.webp'
 import jacket3 from '../img/jacket3.webp'
 import jacket4 from '../img/jacket4.jpg'
 import jacket5 from '../img/32.jpeg'
+import ProductContext from "../proudctpage/ProductContext";
 function Show() {
-  const [imageURL, setImageURL] = useState([]);
+    // const [imageURL, setImageURL] = useState([]);
+    const { selectedProduct } = useContext(ProductContext);
 
   return (
     <div className="imgs-conta">
       <div className="big-img">
-        <img src={jacket1} alt="Description" />
+        <img src={selectedProduct.imageURL} alt="Description" />
       </div>
       <div className="small-img">
         <img src={jacket2} alt="Description" />
--- src/proudctpage/proudctData.js
+++ src/proudctpage/proudctData.js
@@ -1,6 +1,5 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-
 import ProductContext from ".//ProductContext";
 import "./proudctData.css";
 
